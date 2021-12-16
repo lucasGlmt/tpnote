7c0cbbcd6820d3e8b0ef245d710d8648a6a6d214
lucas.guilleminot <lcs.glmt@gmail.com>
2021-12-16 16:58:43 +0100

    map

--- Map.js
+++ Map.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
 import MapView from 'react-native-maps';
 
 const MapScreen = () => {
+  const [lat, setLat] = useState(0);
+
   return (
-    <View>
-      <View></View>
-      <MapView style={styles.map} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Text>Quelle endroit voulez-vous afficher ?</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput value={lat} placeholder="Lat" style={styles.input} />
+        </View>
+      </View>
+      <View style={{ flex: 3 }}>
+        <MapView style={styles.map} />
+      </View>
+    </SafeAreaView>
   );
 };
 
-const HeaderMap = () => {
-  return <View></View>;
-};
-
 const styles = StyleSheet.create({
+  input: {
+    borderColor: 'gray',
+  },
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   map: {
     width: Dimensions.get('window').width,
